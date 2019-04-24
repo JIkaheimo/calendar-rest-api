02a94331705ccf63bcc69fafd104b9b30e6f9e49
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2019-04-24 22:56:32 +0300

    Add default routing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 const express = require('express');
 
 const app = express();
+const port = process.env.port || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Test!');
+  res.end();
+});
+
+app.listen(port);
